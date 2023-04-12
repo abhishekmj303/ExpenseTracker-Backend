0437b4c4e33ed4b2e664fbd2fbfb2208316495db
Abhishek M J <abhishekmj303@gmail.com>
2023-04-12 21:56:24 +0530

    Add user-auth and update models

--- app.py
+++ app.py
@@ -2,31 +2,123 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
+import flask_praetorian
+from flask_mail import Mail, Message
 from flask_cors import CORS
 from models import *
 
 load_dotenv()
 
 app = Flask(__name__)
+
+# Enable CORS
 CORS(app)
 
+# Setup database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.getcwd(), 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# Setup user-authentication (https://flask-praetorian.readthedocs.io/en/latest/flask_praetorian.html)
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+app.config["JWT_ACCESS_LIFESPAN"] = {"hours": 24}
+app.config["JWT_REFRESH_LIFESPAN"] = {"days": 30}
+app.config["PRAETORIAN_ROLES_DISABLED"] = True
+app.config["PRAETORIAN_CONFIRMATION_SENDER"] = os.getenv('EMAIL')
+guard = flask_praetorian.Praetorian()
+with app.app_context():
+    guard.init_app(app, User)
 
-@app.route('/')
+
+# Setup mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL')
+app.config['MAIL_PASSWORD'] = os.getenv('PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+
+@app.route('/login', methods=['POST'])
+def login_api():
+    req = request.get_json(force=True)
+    username = req.get("username", None)
+    password = req.get("password", None)
+    user = guard.authenticate(username, password)
+    if not user:
+        return jsonify({"message": "Invalid credentials"}), 401
+    if not user.is_confirm:
+        return jsonify({"message": "Please confirm your account"}), 401
+    resp = {"access_token": guard.encode_jwt_token(user), "message": "Login successful"}
+    return jsonify(resp), 200
+
+
+@app.route('/signup', methods=['POST'])
+def signup_api():
+    req = request.get_json(force=True)
+    req['hashed_password'] = guard.hash_password(req.get('password', None))
+    del req['password']
+    new_username = req.get('username', None)
+    if User.query.filter_by(username=new_username).first():
+        resp = {'message': f'username {new_username} already exists'}
+        return jsonify(resp), 409
+    new_user = User(**req)
+    db.session.add(new_user)
+    db.session.commit()
+    guard.send_registration_email(
+        new_user.email, new_user,
+        subject="Confirm your account",
+        confirmation_uri="http://localhost:5000/verify"
+    )
+    resp = {'message': f'successfully sent registration email to user {new_user.email}'}
+    return jsonify(resp), 200
+
+
+@app.route('/verify', methods=['GET'])
+def verify_api():
+    token = request.args.get('token', None)
+    if not token:
+        return jsonify({"message": "Missing token"}), 400
+    try:
+        user = guard.get_user_from_registration_token(token)
+        if user.is_confirm:
+            return jsonify({"message": "User already confirmed"}), 400
+        User.query.filter_by(id=user.id).update({'is_confirm': True})
+        db.session.commit()
+        resp = {'message': f'user {user.username} successfully confirmed'}
+        return jsonify(resp), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+
+# def refresh_jwt(token):
+#     try:
+#         user = guard.get_user_from_refresh_token(token)
+#         return guard.encode_jwt_token(user)
+#     except Exception as e:
+#         return jsonify({"message": str(e)}), 400
+
+
+@app.route('/mail')
 def home():
-    return "Hello!!"
+    msg = Message('Hello', sender=os.getenv('EMAIL'), recipients=['siddeshdslr@gmail.com', 'abhishekmj303@gmail.com'])
+    msg.body = "This is a test email sent using Flask-Mail."
+    mail.send(msg)
+    return "Sent"
 
 
-@app.route('/expense/<int:expense_id>', methods=['GET', 'POST', 'DELETE'])
-def expense_api(expense_id):
+@app.route('/expense', methods=['GET', 'POST', 'DELETE'])
+@flask_praetorian.auth_required
+def expense_api():
+    user = flask_praetorian.current_user()
     if request.method == 'GET':
-        expense = Expense.query.filter_by(id=expense_id).first()
-        return jsonify(expense), 200
+        expenses = Expense.query.filter(
+            (Expense.username == user.username) |
+            ((Expense.other_username == user.username) & (Expense.is_private == False))
+        ).all()
+        return jsonify(expenses), 200
     elif request.method == 'POST':
         data = request.get_json()
         expense = Expense(**data)
@@ -35,8 +127,17 @@ def expense_api(expense_id):
         resp = {'id': expense.id, 'message': 'Expense added', 'status': 'success'}
         return jsonify(resp), 201
     elif request.method == 'DELETE':
-        expense = Expense.query.filter_by(id=expense_id).first()
+        expense = Expense.query.filter().first()
         db.session.delete(expense)
         db.session.commit()
-        resp = {'id': expense_id, 'message': 'Expense deleted', 'status': 'success'}
+        resp = {'message': 'Expense deleted', 'status': 'success'}
         return jsonify(resp), 200
+
+
+@app.route('/event', methods=['GET', 'POST', 'DELETE'])
+@flask_praetorian.auth_required
+def event_api():
+    if request.method == 'GET':
+        user = flask_praetorian.current_user()
+        events = Event.query.filter().all()
+        return jsonify(events), 200
--- models.py
+++ models.py
@@ -1,17 +1,55 @@
 from datetime import datetime
 from dataclasses import dataclass
 from flask_sqlalchemy import SQLAlchemy
+# from flask_praetorian import SQLAlchemyUserMixin
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
     __tablename__ = 'user'
-    username = db.Column(db.String(80), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    password_hashed = db.Column(db.String(255), nullable=False)
+    hashed_password = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(80))
     upi = db.Column(db.String(30))
+    date_joined = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    is_confirm = db.Column(db.Boolean, default=False, server_default="false")
+    is_active = db.Column(db.Boolean, default=True, server_default="true")
+
+    events = db.relationship('Event', secondary='event_user', back_populates='users')
+    transactions = db.relationship('Transaction', secondary='transaction_user', back_populates='users')
+
+    @property
+    def identity(self):
+        return self.id
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @property
+    def rolenames(self):
+        return []
+
+    @classmethod
+    def lookup(cls, username):
+        return cls.query.filter_by(username=username).one_or_none()
+
+    @classmethod
+    def identify(cls, user_id):
+        return cls.query.get(user_id)
+
+    def is_valid(self):
+        return self.is_active
+
+
+event_user = db.Table(
+    'event_user',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 
 class Event(db.Model):
@@ -19,28 +57,39 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), default='Untitled Event', nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    is_private = db.Column(db.Boolean, default=False, nullable=False)
+    transactions = db.relationship('Transaction', back_populates='event')
+    users = db.relationship('User', secondary=event_user, back_populates='events')
 
 
-class EventUser(db.Model):
-    __tablename__ = 'event_user'
-    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), primary_key=True)
-    username = db.Column(db.String(80), db.ForeignKey('user.username'), primary_key=True)
+# class EventUser(db.Model):
+#     __tablename__ = 'event_user'
+#     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), primary_key=True)
+#     username = db.Column(db.String(80), db.ForeignKey('user.username'), primary_key=True)
 
 
-class EventTransaction(db.Model):
-    __tablename__ = 'event_transaction'
+transaction_user = db.Table(
+    'transaction_user',
+    db.Column('transaction_id', db.Integer, db.ForeignKey('transaction.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('amount', db.Float, nullable=False)
+)
+
+
+class Transaction(db.Model):
+    __tablename__ = 'transaction'
     id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
     name = db.Column(db.String(80), default='Untitled', nullable=False)
     category = db.Column(db.String(80), default='General', nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    event = db.relationship('Event', back_populates='transactions')
+    users = db.relationship('User', secondary=transaction_user, back_populates='transactions')
 
 
-class TransactionUser(db.Model):
-    __tablename__ = 'transaction_user'
-    transaction_id = db.Column(db.Integer, db.ForeignKey('event_transaction.id'), primary_key=True)
-    username = db.Column(db.String(80), db.ForeignKey('user.username'), primary_key=True)
-    amount = db.Column(db.Float, nullable=False)
+# class TransactionUser(db.Model):
+#     __tablename__ = 'transaction_user'
+#     transaction_id = db.Column(db.Integer, db.ForeignKey('event_transaction.id'), primary_key=True)
+#     username = db.Column(db.String(80), db.ForeignKey('user.username'), primary_key=True)
+#     amount = db.Column(db.Float, nullable=False)
 
 
 @dataclass
